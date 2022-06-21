7e7c06efe972061159b1dd1424f73242e1ac7021
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-22 01:04:37 +0300

    redaction002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,5 +1,5 @@
 ﻿//added username - input data//
 Console.WriteLine("Введите ваше имя");
 string username = Console.ReadLine();
-Console.WriteLine("Привет, ");
-Console.WriteLine(username);
+Console.WriteLine ("Привет, ");
+Console.WriteLine (username);
