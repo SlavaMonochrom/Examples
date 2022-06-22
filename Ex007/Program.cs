dada56871ce9b14067e36f4c00fd01383ee82fa0
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-22 16:02:17 +0300

    bugsEx007

--- Ex007/Program.cs
+++ Ex007/Program.cs
@@ -1,19 +1,16 @@
 ﻿Console.Clear();
-Console.SetCursorPosition(10,4);
-Console.WriteLine("+");
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
 
-int xa = 1;
-int xb = 1;
-int ya = 1;
-int yb = 30;
-int xc = 40;
-int yc = 30;
+int xa = 20, ya = 1, 
+    xb = 1,  yb = 30,
+    xc = 40, yc = 30;
 
-Console.SetCursorPosition (xa,ya);
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine ("+");
 
-Console.SetCursorPosition (xb,yb);
+Console.SetCursorPosition(xb, yb);
 Console.WriteLine ("+");
 
-Console.SetCursorPosition (xc,yc);
+Console.SetCursorPosition(xc, yc);
 Console.WriteLine ("+");
